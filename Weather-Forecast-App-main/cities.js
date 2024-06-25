3d3d412994379e2168bd0517145dfc38f36fe414
Ayush Srivastava <ayushkrsrivastava12@gmail.com>
2024-06-25 10:36:48 +0530

    Weather App
    The Weather App Project aims to develop a user-friendly mobile application that provides real-time weather updates, detailed forecasts, and severe weather alerts. Leveraging advanced meteorological data and intuitive design, the app will offer users accurate and timely information to help them plan their day, week, or travel. Key features include customizable location settings, radar maps, and weather notifications, ensuring users are always informed about current and future weather conditions.

---
+++ Weather-Forecast-App-main/cities.js
@@ -0,0 +1,421 @@
+export default [
+  {
+    country: "IND",
+    name: "Delhi",
+    lat: "18.9750",
+    lng: "72.8258",
+  }
+  ,
+  {
+    country: "IND",
+    name: "Bangalore",
+    lat: "12.9716",
+    lng: " 77.5946",
+  }
+  ,
+  {
+    country: "IND",
+    name: "Hyderabad",
+    lat: "17.3850",
+    lng: "78.4867",
+  }
+  ,
+  {
+    country: "IND",
+    name: "Ahmedabad",
+    lat: "23.0225",
+    lng: "72.5714",
+  }
+  ,
+  {
+    country: "IND",
+    name: "Chennai",
+    lat: " 13.0827",
+    lng: "80.2707",
+  }
+  ,
+  {
+    country: "IND",
+    name: "Kolkata",
+    lat: "22.5726",
+    lng: "88.3639",
+  }
+  ,{
+    country: "IND",
+    name: "Surat",
+    lat: "21.1702",
+    lng: "72.8311",
+  }
+  ,{
+    country: "IND",
+    name: "Pune",
+    lat: "18.5204",
+    lng: "73.8567",
+  }
+  ,{
+    country: "IND",
+    name: "Jaipur",
+    lat: "26.9124",
+    lng: "75.7873",
+  }
+  ,{
+    country: "IND",
+    name: "Lucknow",
+    lat: "26.8467",
+    lng: "80.9462",
+  }
+  ,{
+    country: "IND",
+    name: "Kanpur",
+    lat: "26.4499",
+    lng: "80.3319",
+  }
+  ,{
+    country: "IND",
+    name: "Nagpur",
+    lat: "21.1458",
+    lng: "79.0882",
+  }
+  ,{
+    country: "IND",
+    name: "Indore",
+    lat: "22.7196",
+    lng: "75.8577",
+  }
+  ,{
+    country: "IND",
+    name: "Thane",
+    lat: "19.2183",
+    lng: "72.9781",
+  }
+  ,{
+    country: "IND",
+    name: "Bhopal",
+    lat: "23.2599",
+    lng: "77.4126",
+  }
+  ,{
+    country: "IND",
+    name: "Visakhapatnam",
+    lat: "17.6868",
+    lng: "83.2185",
+  }
+  ,{
+    country: "IND",
+    name: "Patna",
+    lat: "25.5941",
+    lng: "85.1376",
+  }
+  ,{
+    country: "IND",
+    name: "Vadodara",
+    lat: "22.3072",
+    lng: "73.1812",
+  }
+  ,{
+    country: "IND",
+    name: "Ghaziabad",
+    lat: "28.6692",
+    lng: "77.4538",
+  }
+  ,{
+    country: "IND",
+    name: "Ludhiana",
+    lat: "30.9010",
+    lng: "75.8573",
+  }
+  ,{
+    country: "IND",
+    name: "Agra",
+    lat: "27.1767",
+    lng: "78.0081",
+  }
+  ,{
+    country: "IND",
+    name: "Nashik",
+    lat: "19.9975",
+    lng: "73.7898",
+  }
+  ,{
+    country: "IND",
+    name: "Faridabad",
+    lat: "28.4089",
+    lng:"77.3178",
+  }
+  ,{
+    country: "IND",
+    name: "Meerut",
+    lat: "28.9845",
+    lng:"77.7064",
+  }
+  ,{
+    country: "IND",
+    name: "Rajkot",
+    lat: "22.3039",
+    lng:"70.8022",
+  }
+  ,{
+    country: "IND",
+    name: "Varanasi",
+    lat: "25.3176",
+    lng:"82.9739",
+  }
+  ,{
+    country: "IND",
+    name: "Srinagar",
+    lat: "34.0837",
+    lng:"74.7973",
+  }
+  ,{
+    country: "IND",
+    name: "Allahabad",
+    lat: "25.4358",
+    lng:"81.8463",
+  }
+  ,{
+    country: "IND",
+    name: "Ranchi",
+    lat: "23.3441",
+    lng:"85.3096",
+  }
+  ,{
+    country: "IND",
+    name: "Kota",
+    lat: "25.2138",
+    lng:"75.8648",
+  }
+  ,{
+    country: "IND",
+    name: "Mysore",
+    lat: "12.2958",
+    lng:"76.6394",
+  }
+  ,{
+    country: "IND",
+    name: "Gaya",
+    lat: "24.7914",
+    lng:"85.0002",
+  }
+  ,{
+    country: "IND",
+    name: "Muzaffarpur",
+    lat: "26.1209",
+    lng:"85.3647",
+  }
+  ,{
+    country: "IND",
+    name: "Bihar",
+    lat: "25.0961",
+    lng:"85.3131",
+  }
+  ,{
+    country: "IND",
+    name: "Andhra Pradesh",
+    lat: "15.9129",
+    lng:"79.7400",
+  }
+  ,{
+    country: "IND",
+    name: "Arunachal Pradesh",
+    lat:"28.2170",
+    lng:"94.7278",
+  }
+  ,{
+    country: "IND",
+    name: "Assam",
+    lat:"26.2006",
+    lng:"92.9376",
+  }
+  ,{
+    country: "IND",
+    name: "Chhattisgarh",
+    lat:"21.2787",
+    lng:"81.8661",
+  }
+  ,{
+    country: "IND",
+    name: "Goa",
+    lat:"15.2993",
+    lng:"74.1240",
+  }
+  ,{
+    country: "IND",
+    name: "Gujarat",
+    lat:"22.2587",
+    lng:"71.1924",
+  }
+  ,{
+    country: "IND",
+    name: "Haryana",
+    lat:"29.0588",
+    lng:"76.0856",
+  }
+  ,{
+    country: "IND",
+    name: "Himachal Pradesh",
+    lat:"31.1048",
+    lng:"77.1734",
+  }
+  ,{
+    country: "IND",
+    name: "Jharkhand",
+    lat:"23.6102",
+    lng:"85.2799",
+  }
+  ,{
+    country: "IND",
+    name: "Karnataka",
+    lat:"15.3173",
+    lng:"75.7139",
+  }
+  ,{
+    country: "IND",
+    name: "Kerala",
+    lat:"10.8505",
+    lng:"76.2711",
+  }
+  ,{
+    country: "IND",
+    name: "Madhya Pradesh",
+    lat:"22.9734",
+    lng:"78.6569",
+  }
+  ,{
+    country: "IND",
+    name: "Maharashtra",
+    lat:"19.7515",
+    lng:"75.7139",
+  }
+  ,{
+    country: "IND",
+    name: "Manipur",
+    lat:"24.6637",
+    lng:"93.9063",
+  }
+  ,{
+    country: "IND",
+    name: "Meghalaya",
+    lat:"25.4670",
+    lng:"91.3662",
+  }
+  ,{
+    country: "IND",
+    name: "Mizoram",
+    lat:"23.1645",
+    lng:"92.9376",
+  }
+  ,{
+    country: "IND",
+    name: "Nagaland",
+    lat:"26.1584",
+    lng:"94.5624",
+  }
+  ,{
+    country: "IND",
+    name: "Odisha",
+    lat:"20.9517",
+    lng:"85.0985",
+  }
+  ,{
+    country: "IND",
+    name: "Punjab",
+    lat:"31.1471",
+    lng:"75.3412",
+  }
+  ,{
+    country: "IND",
+    name: "Rajasthan",
+    lat:"27.0238",
+    lng:"74.2179",
+  }
+  ,{
+    country: "IND",
+    name: "Sikkim",
+    lat:"27.5320",
+    lng:"88.5122",
+  }
+  ,{
+    country: "IND",
+    name: "Tamil Nadu",
+    lat:"11.1271",
+    lng:"78.6569",
+  }
+  ,{
+    country: "IND",
+    name: "Telangana",
+    lat:"18.1124",
+    lng:"79.0193",
+  }
+  ,{
+    country: "IND",
+    name: "Tripura",
+    lat:"23.9408",
+    lng:"91.9882",
+  }
+  ,{
+    country: "IND",
+    name: "Uttar Pradesh",
+    lat:"26.8467",
+    lng:"80.9462",
+  }
+  ,{
+    country: "IND",
+    name: "Uttarakhand",
+    lat:"30.0668",
+    lng:"79.0193",
+  }
+  ,{
+    country: "IND",
+    name: "West Bengal",
+    lat:"22.9868",
+    lng:"87.8550",
+  }
+  ,{
+    country: "IND",
+    name: "Andaman and Nicobar Islands",
+    lat:"11.7401",
+    lng:"92.6586",
+  }
+  ,{
+    country: "IND",
+    name: "Chandigarh",
+    lat:"30.7333",
+    lng:"76.7794",
+  }
+  ,{
+    country: "IND",
+    name: "Dadra and Nagar Haveli and Daman and Diu",
+    lat:"20.3974",
+    lng:"72.8328",
+  }
+  ,{
+    country: "IND",
+    name: "Lakshadweep",
+    lat:"10.3280",
+    lng:"72.7846",
+  }
+  ,{
+    country: "IND",
+    name: "Delhi",
+    lat:"28.7041",
+    lng:"77.1025",
+  }
+  ,{
+    country: "IND",
+    name: "Puducherry",
+    lat:"11.9416",
+    lng:"79.8083",
+  }
+  ,{
+    country: "IND",
+    name: "Ladakh",
+    lat:"34.1526",
+    lng:"77.5770",
+  }
+  ,{
+    country: "IND",
+    name: "Jammu and Kashmir",
+    lat:"33.7782",
+    lng:"76.5762",
+  }
+]
